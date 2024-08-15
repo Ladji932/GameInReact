2b0e9625fe5a23393e17f9b1e5363fb5532285b6
Ladji <ladjimaiga@haouas-Air.lan>
2024-08-15 18:39:11 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react';
-import Login from './components/login';
 import SelectCharacter from './components/selectCharacter';
 import Fight from './components/figth'; 
 import data from './assets/data.json';
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [selectedCharacter, setSelectedCharacter] = useState(null);
   const [characters, setCharacters] = useState(data);
-
 
   const redirectCharacter = () => {
     setSelectedCharacter(null);
@@ -16,7 +13,7 @@ const App = () => {
 
   const handleCharacterSelect = (character) => {
     setSelectedCharacter(character);
-    localStorage.setItem('Personnage',JSON.stringify(character))
+    localStorage.setItem('Personnage', JSON.stringify(character));
     console.log('Personnage sélectionné:', character.name);
   };
 
@@ -24,24 +21,16 @@ const App = () => {
     setCharacters(updatedCharacters);
   };
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
   return (
     <div className='app'>
-      {isLoggedIn ? (
-        selectedCharacter ? (
-          <Fight character={selectedCharacter} redirect={redirectCharacter} />
-        ) : (
-          <SelectCharacter
-            characters={characters}
-            characterSelect={handleCharacterSelect}
-            onCharacterUpdate={handleCharacterUpdate}
-          />
-        )
+      {selectedCharacter ? (
+        <Fight character={selectedCharacter} redirect={redirectCharacter} />
       ) : (
-        <Login onLogin={handleLogin} />
+        <SelectCharacter
+          characters={characters}
+          characterSelect={handleCharacterSelect}
+          onCharacterUpdate={handleCharacterUpdate}
+        />
       )}
     </div>
   );
